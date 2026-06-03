b5a9fd45f3818ed886ff55bb167c54a6529309b7
SENDE SOUMOUK Ben Fahad <yohannesende@gmail.com>
2026-06-03 20:44:32 +0200

    ajout des nouvelles modifications des fichiers

--- nm-shop/src/main.jsx
+++ nm-shop/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./app.css"; // 👈 Remplace "./style-final.css" par "./app.css" (ou le nom exact du fichier qu'on a créé)
+import "./App.css"; // 👈 Remplace "./style-final.css" par "./app.css" (ou le nom exact du fichier qu'on a créé)
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
